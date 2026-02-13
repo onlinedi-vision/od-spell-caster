92c5c3c18e0a4eb8651097f2e0c320a814d29e34
Ana Alexandru-Gabriel <alexandru.ana@nokia.com>
2026-02-13 17:57:07 +0200

    core: make spell-caster use the new api domain name

--- consts.ts
+++ consts.ts
@@ -1,6 +1,6 @@
-const spell_cast_api = 'https://onlinedi.vision/api/spell/cast';
-const get_user_servers_api = 'https://onlinedi.vision/api/get_user_servers'
-const am_i_in_server_api = 'https://onlinedi.vision/api/am_i_in_server'
+const spell_cast_api = 'https://api.onlinedi.vision/api/spell/cast';
+const get_user_servers_api = 'https://api.onlinedi.vision/api/get_user_servers'
+const am_i_in_server_api = 'https://api.onlinedi.vision/api/am_i_in_server'
 const port = 6666;
 
 module.exports = {
